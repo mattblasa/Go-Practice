f917a83c84b34db17e9ac6346e9fa3315c4e0de0
mattblasa <blasa.matthew@yahoo.com>
2021-03-07 17:43:07 -0600

    Add Equivalency
    Short variables in Go

---
+++ equivalency.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//This is a short variable declaration 
+	n := 1 == 1
+	n := 1 == 2
+	fmt.Printf("%v, %T", n, n )
+}
--- primitives.go
+++ primitives.go
@@ -5,8 +5,9 @@ import (
 )
 
 func main() {
-	//var defines a variable, n is the name, and bool is thype
+	//var defines a variable, n is the name, and bool is type
 	var n bool = true
-	
+	//string is the template. Prinf means print format.
+	//%v means default format, %T means type of value, \n means U+000A line feed or newline
 	fmt.Printf("%v, %T\n", n, n)
 }
