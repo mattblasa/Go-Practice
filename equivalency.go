05c2c2d08519e7081ca852c598728b5dae04a094
mattblasa <blasa.matthew@yahoo.com>
2021-03-07 18:04:37 -0600

    Added quivalency

--- equivalency.go
+++ equivalency.go
@@ -6,9 +6,10 @@ import (
 
 func main() {
 	//This is a short variable declaration
+	// Variables in GO are automatically set to 0
 	n := 1 == 1
 	m := 1 == 2
-	//\n makes a line skip 
+	//\n makes a line skip
 	fmt.Printf("%v, %T\n", n, n )
 	fmt.Printf("%v, %T", m, m)
 }
---
+++ numerical_learning.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 10 // 1010
+	b := 3 // 0011
+	fmt.Println(a & b) // 0010 = 2
+	fmt.Println(a | b) // OR 1011 =11
+	fmt.Println(a ^ b) //
+	fmt.Println(a &^ b)
+}
