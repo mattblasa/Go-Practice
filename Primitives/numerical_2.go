bb4c84f55df32cc0778d11e364b20252cda0d140
mattblasa <blasa.matthew@yahoo.com>
2021-03-27 21:05:00 -0500

    Added Primtive Folder

--- Primitives/numerical_2.go
+++ Primitives/numerical_2.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 )
+
 /*
 Bitshifting is occurring here.
- */
+*/
 func main() {
 	//short declaration operator. This stores 8 in a variable, without declaring it
-	a := 8 // 8^3
+	a := 8              // 8^3
 	fmt.Println(a << 3) //n times 2, x times
 	fmt.Println(a >> 3) // "y divided by 2, z times"
 }
