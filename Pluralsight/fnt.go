fecd64e792f428635a5b834ddb570d14d995fddb
mattblasa <blasa.matthew@yahoo.com>
2021-04-30 21:01:40 -0500

    Added PluralSight section

---
+++ Pluralsight/fnt.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int
+	i = 42
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	firstName := "Arthur"
+	fmt.Println(firstName)
+
+	b := true
+	fmt.Println(b)
+}
+
