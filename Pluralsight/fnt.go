0bcd412b39ef6779a36b49120ea85247584bc66a
mattblasa <blasa.matthew@yahoo.com>
2021-04-30 21:48:18 -0500

    Update fnt.go

--- Pluralsight/fnt.go
+++ Pluralsight/fnt.go
@@ -17,5 +17,13 @@ func main() {
 
 	b := true
 	fmt.Println(b)
+
+	//Store imaginary numbers using the complex method
+	c := complex(3,4)
+	fmt.Println(c)
+
+	//Declare real and imaginary values of variable c 
+	r, im := real(c), imag(c)
+	fmt.Println(r, im)
 }
 
