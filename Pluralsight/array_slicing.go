8d3619833e229c76a43044ecd10d63fa0411a573
mattblasa <blasa.matthew@yahoo.com>
2021-05-13 19:57:38 -0500

    Update array_slicing.go

--- Pluralsight/array_slicing.go
+++ Pluralsight/array_slicing.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := [3]int{1, 2, 3}
+
+	slice := arr[:] // the colon in the middle means that I want slice from the beginning of the connection to the end
+
+	fmt.Println(arr, slice)
+}
