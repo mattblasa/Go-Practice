215537eb7fe4f3ea785a913995128eba1c92b85a
mattblasa <blasa.matthew@yahoo.com>
2021-05-13 20:25:55 -0500

    Added Mapping Data Types

--- Pluralsight/array_slicing_NotExplicit.go
+++ Pluralsight/array_slicing_NotExplicit.go
@@ -6,7 +6,14 @@ func main() {
 	slice := []int{1, 2, 3} //This creates the array, and slices at the same time.
 	fmt.Println(slice) //slice is not a fixed sized entity.
 
-	slice = append(slice, 4, 96, 22, 33)
+	slice = append(slice, 4, 96, 22, 33) //Go manages the resizing of the array
 	fmt.Println(slice) //slice is not a fixed sized entity.
+
+	s2 := slice[1:] //slice index 1
+	s3 := slice[:2]
+	s4 := slice[1:2]
+
+	fmt.Println(s2, s3, s4)
+
 }
 
