1308e6487401d807e70647b3bbbfb3dfa02bf7b9
mattblasa <blasa.matthew@yahoo.com>
2021-06-07 22:17:36 -0500

    Delete Duplocate of Structs.go

--- Structs.go
+++ Structs.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	type user struct {
+		ID int
+		FirstName string
+		LastName string
+	}
+	var u user  //Initalization syntax of an object
+	u.ID = 1
+	u.FirstName = "Arthur"
+	u.LastName = "Dent"
+	fmt.Println(u.FirstName)
+}
