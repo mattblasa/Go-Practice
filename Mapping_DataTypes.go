7e2b542ecabdc232da726913bec671a8bc40470e
mattblasa <blasa.matthew@yahoo.com>
2021-06-07 21:51:34 -0500

    Update Mapping_DataTypes.go

--- Mapping_DataTypes.go
+++ Mapping_DataTypes.go
@@ -5,5 +5,13 @@ import (
 )
 
 func main() {
-	m := map[string]int{"foo" : 42} //specifies a map of one element that has a key of 'foo' and number 42 
+	m := map[string]int{"foo" : 42} //specifies a map of one element that has a key of 'foo' and number 42
+	fmt.Println(m)
+	fmt.Println(m["foo"])
+
+	m["foo"] = 27
+	fmt.Println(m)
+
+	delete(m, "foo") // deletes key foo
+	fmt.Println(m)
 }
