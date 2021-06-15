b4bb7658369741aacffe300cf658a406941e225c
mattblasa <blasa.matthew@yahoo.com>
2021-06-14 20:02:15 -0500

    Create functions.go

---
+++ Pluralsight/functions.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Yo killa.")
+}
+
+func startWebServer() {
+	fmt.Println("Starting server...")
+	//Stuff here
+	fmt.Println("Server started")
+}
