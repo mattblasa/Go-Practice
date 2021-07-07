d422869f5eea933290a05ccb1b37cd1f36248482
mattblasa <blasa.matthew@yahoo.com>
2021-07-06 23:15:07 -0500

    Update user.go :  Constructs Example

--- Pluralsight/Controllers/user.go
+++ Pluralsight/Controllers/user.go
@@ -1,7 +1,13 @@
 package Controllers
 
+import "net/http"
+
 //Custom data type userController
-type userController struct {}
+type userController struct{
+
+}
 
 //method. userController is the type for uc 
-func (uc userController)
+func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Plano!") //Convert string to byte slice
+}
