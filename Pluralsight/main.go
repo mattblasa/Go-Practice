e93b88ad73de810e16bcf1f0714232624128ac77
mattblasa <blasa.matthew@yahoo.com>
2021-07-07 22:38:37 -0500

    Update main.go

--- Pluralsight/main.go
+++ Pluralsight/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/pluralsight/webservice/models"
+)
+
+func main() {
+	controllers.RegisterController()
+	//IP address we are listening on
+	http.ListenAndServe(":3000", nil)
+}
