5712e4b726d1149a3c319425c1c8d3a13bb8e3e0
mattblasa <blasa.matthew@yahoo.com>
2021-07-21 22:27:38 -0500

    Added Loop Experiments

---
+++ Pluralsight/Experiments/loops.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+//Declare array
+var array = []int{3, 5, -4, 8, 11, 1, -1, 6}
+var target int = 10
+func main() {
+	for i := 0; i < 20; i++ {
+		target += i
+	}
+	fmt.Println(target)
+}
+
