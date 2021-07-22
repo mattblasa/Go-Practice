f98d6ffd6243f6cf9adec885e3a91c9471d01db3
mattblasa <blasa.matthew@yahoo.com>
2021-07-21 22:49:16 -0500

    Update loops3.go

--- Pluralsight/Experiments/loops3.go
+++ Pluralsight/Experiments/loops3.go
@@ -5,9 +5,12 @@ import "fmt"
 //Declare array
 var array = []int{3, 5, -4, 8, 11, 1, -1, 6}
 var target int = 10
+
 func main() {
-	for i := 0; i < 10; i++ {
-		fmt.Println(target)
+	for i := 0; i < len(array)-1; i++ {
+		//This prints each number in the array
+		firstNum := array[i]
+		fmt.Println(firstNum)
 	}
 }
 
